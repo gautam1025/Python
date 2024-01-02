f4d1976ebf4db1c402a7c1947430359a4d1e1bff
Gautam Gupta <123723190+gautam1025@users.noreply.github.com>
2024-01-02 19:19:08 +0530

    Add files via upload

---
+++ Taking-user-input.py
@@ -0,0 +1,13 @@
+a=input("Enter your name\n")
+print("Your name is ",a )
+
+x=int(input("Enter 1st number: "))
+y=int(input("Enter 2nd number: "))
+print(x+y)
+                    # OR
+x=input("Enter 1st number: ")
+y=input("Enter 2nd number: ")
+print(int(x)+int(y))
+
+
+ 
---
+++ Typecasting-in-python.py
@@ -0,0 +1,12 @@
+# Changing strings into integer via coding
+# (i.e. explicit typecasting)
+a="45"
+b="65"
+print(a+b)
+print(int(a)+int(b))
+
+# Automatic typecasting (i.e. implicit typecasting)
+c=8.14
+d=45
+print(c+d)
+print(str(c)+str(d))
