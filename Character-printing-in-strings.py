4230be703ed8d1ba3466906258542b0ddb3ad16a
Gautam Gupta <123723190+gautam1025@users.noreply.github.com>
2024-01-03 18:57:14 +0530

    Python programs

---
+++ Character-printing-in-strings.py
@@ -0,0 +1,12 @@
+name = "Gautam Gupta"
+print(name[8])
+print("is the character")
+line = '''\nCow, in common parlance, 
+a domestic bovine, 
+regardless of sex and age, 
+usually of the species Bos taurus.
+'''
+# Using for loop for printing characters in strings
+ 
+for characters in line :
+    print(characters)
