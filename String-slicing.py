d7a8ec9e061a9fba4a4e16893e027462854fee71
Gautam Gupta <123723190+gautam1025@users.noreply.github.com>
2024-01-03 19:42:18 +0530

    Python programs

--- String-slicing.py
+++ String-slicing.py
@@ -4,5 +4,9 @@ print(lenght)
 print(name[0:4]) # including 0 but not 4
 print(name[1:4]) # including 1 but not 4
 print(name[:]) # gives the complete string
-
+print(name[-8:-4])
+           # OR
+print(name[len(name)-8:len(name)-4])
+# Both the above statements gives the same output 
+# This is how the negative slicing works
 
