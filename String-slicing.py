0b2f6d2f01fedee39406fcd77fea2141e51dae54
Gautam Gupta <123723190+gautam1025@users.noreply.github.com>
2024-01-03 19:34:17 +0530

    -

---
+++ String-slicing.py
@@ -0,0 +1,8 @@
+name="Gautam gupta"
+lenght=(len(name))
+print(lenght)
+print(name[0:4]) # including 0 but not 4
+print(name[1:4]) # including 1 but not 4
+print(name[:]) # gives the complete string
+
+
