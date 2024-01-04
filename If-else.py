eb66cf35e8f34a8d33f57f514845c7e56e767568
Gautam Gupta <123723190+gautam1025@users.noreply.github.com>
2024-01-04 23:13:23 +0530

    Python Programs

---
+++ If-else.py
@@ -0,0 +1,15 @@
+a=int(input("Enter your age: "))
+print("Your age is: ")
+# Conditional Operators
+# >, <, <=, >=, ==, != 
+print(a>18)
+print(a<18)
+print(a>=18)
+print(a<=18)
+print(a==18)
+print(a!=18)
+
+if(a>18):
+    print("Yes\nYou can vote")
+else:
+    print("No\nYou cannot vote")
---
+++ Nested-If.py
@@ -0,0 +1,9 @@
+number =int(input("Enter a number: "))
+if (number >= 1):
+    if number == 0:
+      print('Number is 0')
+    else:
+        print('Number is positive')
+else:
+    print('Number is negative')
+
