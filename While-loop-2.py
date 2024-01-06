2c54d5ddcfb1e90a2534b671ccc813ba0035bd3e
Gautam Gupta <123723190+gautam1025@users.noreply.github.com>
2024-01-06 21:52:23 +0530

    Python Programs

---
+++ While-loop-2.py
@@ -0,0 +1,23 @@
+i=1
+a=int(input("Enter number to print square: "))
+while(i<=a):
+    print(i*i)
+    i=i+1
+    
+count=int(input("Enter number to print upto 0: "))
+while(count>0):
+    print(count)
+    count=count-1
+else:
+    print("End of while loop")
+
+# Do while loop is a loop which is executed 
+# atleast once and then check whether the condition
+# is true or not and then further executed
+    
+# Syntax for do while loop is below
+''' do {
+task
+} 
+while (condition):
+    task'''
---
+++ While-loops.py
@@ -0,0 +1,10 @@
+# Use of for loop
+for i in range(5):
+    print(i)
+
+# Use of while loop for above program
+i=0
+while(i<5):
+    print(i)
+    i = i+1
+
