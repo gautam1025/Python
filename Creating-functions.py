bfae2eaf06248510be9e47b369afc0fc6686527c
Gautam Gupta <123723190+gautam1025@users.noreply.github.com>
2024-01-07 19:49:02 +0530

    Python Programs

---
+++ Creating-functions.py
@@ -0,0 +1,10 @@
+def average(a,b,c,d):
+    mean=(a+b+c+d)/4
+    print("The average of above numbers is",mean)
+
+print("Enter 4 numbers to calculate average: ")
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+average(a,b,c,d)
---
+++ Creating-functions2.py
@@ -0,0 +1,8 @@
+# This functon is for calculating the area of a given radius
+
+def area(a):
+    area1=3.14*a*a
+    print("The area of given radius is",area1)
+
+radius=int(input("Enter the radius: "))
+area(radius)
